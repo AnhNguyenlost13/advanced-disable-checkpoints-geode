0356538a2474fb3f8c41f427ade32c32ab3b2ec0
Anh Nguyen <94160753+AnhNguyenlost13@users.noreply.github.com>
2025-05-31 10:13:23 +0700

    Should be it.

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,12 @@ using namespace geode::prelude;
 
 int checkpointCount = 0;
 int notificationCount = 0;
+auto mode = Mod::get()->getSettingValue<std::string>("mode");
 
 $execute {
-	listenForSettingChanges("mode", [](const std::string& mode)
+	listenForSettingChanges("mode", [](const std::string& modeSetting)
 	{
+		mode = modeSetting; // save the mode for later use
 		if (mode == "Disabled") Mod::get()->setSavedValue<bool>("meow", false);
 		else Mod::get()->setSavedValue<bool>("meow", true);
 
@@ -40,39 +42,35 @@ class $modify(PL, PlayLayer)
 
 		bool doDisableCheckpoints = false;
 
-		const bool timeConditionUnsatisfied = Mod::get()->getSettingValue<std::string>("mode").contains("Time") && Mod::get()->getSettingValue<float>("time") > m_gameState.m_levelTime;
-		const bool checkpointConditionUnsatisfied = Mod::get()->getSettingValue<std::string>("mode").contains("Checkpoint") && checkpointCount < Mod::get()->getSettingValue<int>("checkpoint-count");
+		const bool timeConditionUnsatisfied = mode.contains("Time") && Mod::get()->getSettingValue<float>("time") > m_gameState.m_levelTime;
+		const bool checkpointConditionUnsatisfied = mode.contains("Checkpoint") && checkpointCount < Mod::get()->getSettingValue<int>("checkpoint-count");
 
-		if (Mod::get()->getSettingValue<std::string>("mode").contains("&")) doDisableCheckpoints = timeConditionUnsatisfied && checkpointConditionUnsatisfied;
-		else if (Mod::get()->getSettingValue<std::string>("mode").contains("|")) doDisableCheckpoints = timeConditionUnsatisfied || checkpointConditionUnsatisfied;
+		if (mode.contains("&")) doDisableCheckpoints = timeConditionUnsatisfied && checkpointConditionUnsatisfied;
+		else if (mode.contains("/")) doDisableCheckpoints = timeConditionUnsatisfied || checkpointConditionUnsatisfied;
 
 		if (doDisableCheckpoints)
 		{
-			log::info("doDisableCheckpoints!");
 			GameManager::get()->setGameVariable("0046", doDisableCheckpoints);
 			removeAllCheckpoints(); // Purge all checkpoints if minimum checkpoint count is not reached
 		}
 		checkpointCount = 0;
 	}
 
-	// i'm honestly sick of living this life
 	$override void updateTimeLabel(const int seconds, const int milliseconds, const bool useAlternateFormatting) override
 	{
 		PlayLayer::updateTimeLabel(seconds, milliseconds, useAlternateFormatting);
 
-		// break out early if notification functionality is disabled/level is not platformer
-		if (!m_level->isPlatformer() || !Mod::get()->getSettingValue<bool>("notification") || Mod::get()->getSettingValue<std::string>("mode") == "Disabled" || notificationCount > 0) return;
+		if (!m_level->isPlatformer() || !Mod::get()->getSettingValue<bool>("notification") || mode == "Disabled" || notificationCount > 0) return;
 		notificationCount++;
 
-		// build string
 		std::vector<std::string> shards;
-		if (Mod::get()->getSettingValue<std::string>("mode").contains("Time")) shards.push_back(fmt::format("{} seconds", Mod::get()->getSettingValue<float>("time")));
+		if (mode.contains("Time")) shards.push_back(fmt::format("{} seconds", Mod::get()->getSettingValue<float>("time")));
 		std::string comparator;
-		if (Mod::get()->getSettingValue<std::string>("mode").contains("&")) comparator = "and";
-		else if (Mod::get()->getSettingValue<std::string>("mode").contains("/")) comparator = "or";
+		if (mode.contains("&")) comparator = "and";
+		else if (mode.contains("/")) comparator = "or";
 
 		int checkpointCount = Mod::get()->getSettingValue<int>("checkpoint-count");
-		if (Mod::get()->getSettingValue<std::string>("mode").contains("Checkpoint")) {
+		if (mode.contains("Checkpoint")) {
 			shards.push_back(fmt::format("{} checkpoints", // there has to be at least 2 checkpoints anyway
 				checkpointCount
 			));
@@ -80,19 +78,19 @@ class $modify(PL, PlayLayer)
 
 		if (!shards.empty())
 		{
-			std::string notification = "Checkpoints enabled after ";
+			std::string notificationContent = "Checkpoints enabled after ";
 
 			if (shards.size() == 1)
-				notification += shards[0];
+				notificationContent += shards[0];
 			else if (shards.size() == 2)
-				notification += fmt::format("{} {} {}", shards[0], comparator, shards[1]);
+				notificationContent += fmt::format("{} {} {}", shards[0], comparator, shards[1]);
 			else
 			{
-				for (size_t i = 0; i < shards.size() - 1; i++) notification += fmt::format("{}, ", shards[i]);
-				notification += fmt::format("{} {}", comparator, shards.back());
+				for (size_t i = 0; i < shards.size() - 1; i++) notificationContent += fmt::format("{}, ", shards[i]);
+				notificationContent += fmt::format("{} {}", comparator, shards.back());
 			}
-			Notification* x = Notification::create(notification, NotificationIcon::Info, Mod::get()->getSettingValue<float>("notification-time"));
-			x->show();
+			Notification* notification = Notification::create(notificationContent, NotificationIcon::Info, Mod::get()->getSettingValue<float>("notification-time"));
+			notification->show();
 		}
 	};
 };
